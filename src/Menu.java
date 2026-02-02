cfd617c932b5181f83a11a6044e113491502d8f8
Nattan <natanaelsouzza123@gmail.com>
2026-02-02 09:03:12 -0300

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -8,7 +8,7 @@ public class Menu {
 
         Scanner scam = new Scanner(System.in);
 
-        System.out.println("1 - Listar Quartos Disponiveis.");
+        System.out.println("1 - Listar Quartos.");
         System.out.println("2 - Fazer Reserva.");
         System.out.println("3 - Fazer Check-in.");
         System.out.println("4 - Fazer Check-out.");
