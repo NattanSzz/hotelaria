3eb055f0662acad9f4625731a3e2f1d06d416bbe
Nattan <natanaelsouzza123@gmail.com>
2026-02-03 08:32:33 -0300

    adicionei registro

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         
+        ArrayList<Hospede> hospede = new ArrayList<>();
+
         Quarto[] quarto = {
             new Quarto(100, "simples"),
             new Quarto(101, "duplo"),
@@ -17,6 +22,18 @@ public class Main {
                 for(Quarto Q : quarto){
                     Q.About();
                 }
+            }else if(menu.opcao == 2){
+                Scanner scam = new Scanner(System.in);
+
+                System.out.print("Nome: ");
+                String nome = scam.nextLine();
+                System.out.print("CPF: ");
+                int cpf = scam.nextInt();
+                System.out.print("Telefone: ");
+                int telefone = scam.nextInt();
+
+                hospede.add(new Hospede(nome, cpf, telefone));
+
             }
 
         }
--- src/Menu.java
+++ src/Menu.java
@@ -9,15 +9,16 @@ public class Menu {
         Scanner scam = new Scanner(System.in);
 
         System.out.println("1 - Listar Quartos.");
-        System.out.println("2 - Fazer Reserva.");
-        System.out.println("3 - Fazer Check-in.");
-        System.out.println("4 - Fazer Check-out.");
-        System.out.println("5 - Cancelar Reserva.");
-        System.out.println("6 - Sair.");
+        System.out.println("2 - Registrar-se.");
+        System.out.println("3 - Fazer Reserva.");
+        System.out.println("4 - Fazer Check-in.");
+        System.out.println("5 - Fazer Check-out.");
+        System.out.println("6 - Cancelar Reserva.");
+        System.out.println("7 - Sair.");
 
         
         opcao = scam.nextInt();
-        if(opcao > 0 && opcao <= 6){
+        if(opcao > 0 && opcao <= 7){
             this.opcao = opcao;
         }
 
